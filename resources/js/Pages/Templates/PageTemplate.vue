43b3c4d4cf11c0a135a07c0be54cca314507f305
BlackyDrum <ganiaytan1010@web.de>
2024-01-03 19:49:26 +0100

    Add page template

--- resources/js/Pages/Templates/PageTemplate.vue
+++ resources/js/Pages/Templates/PageTemplate.vue
@@ -4,11 +4,11 @@ import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <Head title="TEMPLATE" />
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">TEMPLATE</h2>
         </template>
 
         <div class="py-12">
