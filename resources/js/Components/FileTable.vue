0310370ad40927a44289ff0be0708b7e12918704
BlackyDrum <ganiaytan1010@web.de>
2024-01-04 00:06:01 +0100

    Add data table component

--- resources/js/Components/FileTable.vue
+++ resources/js/Components/FileTable.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 
-
 import Button from 'primevue/button';
-
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 
 </script>
 
@@ -15,7 +15,12 @@ import Button from 'primevue/button';
             <Button class="text-black border-gray-300 bg-white font-medium" label="Delete" icon="pi pi-trash" />
         </div>
     </div>
-    <div>
-
+    <div class="mt-4">
+        <DataTable tableStyle="min-width: 50rem">
+            <Column field="name" header="Name"></Column>
+            <Column field="owner" header="Owner"></Column>
+            <Column field="last_modified" header="Last Modified"></Column>
+            <Column field="size" header="Size"></Column>
+        </DataTable>
     </div>
 </template>
